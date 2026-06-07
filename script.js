d87e796523319c28638d751c33c23898755723b4
DowardCaparas <caparasdounhuward@gmail.com>
2026-06-07 17:11:59 +0800

    add new item

--- script.js
+++ script.js
@@ -109,6 +109,7 @@ const services = [
   { name: "Name Tag", price: 50, group: "Other" },
   { name: "A4 Name/Subjects Sticker label", price: 50, group: "Other" },
   { name: "Photo Editing Minor Enhancement", price: 30, group: "Other" },
+  { name: "A4 1pc Laminating Film", price: 10, group: "Other" },
 ];
 
 const groupColors = {
